00798aa131a97a71dafccac41df63cea136c3587
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-05-09 16:39:30 -0400

    update homepage

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-
-
 $(document).ready(function() {
     var url = 'http://newsapi.org/v2/top-headlines?' +
     'country=us&' +
     'apiKey=6c1e4b0f26f7483c8541930bb8709d77';
 
     $.get(url).then(function(data) {
+        //var content = document.createTextNode(JSON.stringify(data.articles[0]));
+       $("#para-0").text(data.articles[0].content)
+       $("#img-0").attr("src", data.articles[0].urlToImage)
         console.log(data.articles)
-
-        var xx = document.getElementById("apiText");
-
-//xx.innerHTML += (data.articles);
-var content = document.createTextNode(data.articles);
-
-xx.appendChild(content);
     });
 });
-
    
 
