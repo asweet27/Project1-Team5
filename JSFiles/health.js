4e3d299399c8df14a71772e963c4a7e72f4935f2
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-05-09 20:02:34 -0400

    updated jsand apis

---
+++ JSFiles/health.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    var url = $.getJSON(
+        “https://covidtracking.com/api/v1/states/info.json”,
+        function(data) {
+            console.log(data);
+        }
+    );
+
+    $.get(url).then(function(data) {
+       
+       $("#healthpara-0").text(data.articles[0].content)
+       $("#healthimg-0").attr("src", data.articles[0].urlToImage)
+       $("#health-0").text(data.articles[0].title)
+       $("#healthpara-1").text(data.articles[1].content)
+       $("#healthimg-1").attr("src", data.articles[1].urlToImage)
+       $("#health-1").text(data.articles[1].title)
+       $("#healthpara-2").text(data.articles[2].content)
+       $("#healthimg-2").attr("src", data.articles[2].urlToImage)
+       $("#health-2").text(data.articles[2].title)
+       $("#healthpara-3").text(data.articles[3].content)
+       $("#healthimg-3").attr("src", data.articles[3].urlToImage)
+       $("#health-3").text(data.articles[3].title)
+        console.log(data.articles)
+    });
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+   
--- JSFiles/script.js
+++ JSFiles/script.js
@@ -5,10 +5,32 @@ $(document).ready(function() {
 
     $.get(url).then(function(data) {
         //var content = document.createTextNode(JSON.stringify(data.articles[0]));
+       $("#blog-0").text(data.articles[0].title)
        $("#para-0").text(data.articles[0].content)
        $("#img-0").attr("src", data.articles[0].urlToImage)
+       $("#blog-1").text(data.articles[1].title)
+       $("#para-1").text(data.articles[1].content)
+       $("#img-1").attr("src", data.articles[1].urlToImage)
+       $("#blog-2").text(data.articles[2].title)
+       $("#para-2").text(data.articles[2].content)
+       $("#img-2").attr("src", data.articles[2].urlToImage)
         console.log(data.articles)
+
     });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
    
+
+});
 
