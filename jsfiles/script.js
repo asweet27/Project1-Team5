5eb067b85f07cfb8fe4b83536de72d62b6f37600
Charlie Naranjo <cnara018@fiu.edu>
2020-05-12 19:41:04 -0400

    updated html pages

--- jsfiles/script.js
+++ jsfiles/script.js
@@ -10,21 +10,24 @@ $(document).ready(function() {
      
 				
         $("#blog-0").text(data.articles[0].title)
-        $("#para-0").text(data.articles[0].content)
+        $("#para-0").text(data.articles[0].description)
         $("#img-0").attr("src", data.articles[0].urlToImage)
         $("#link-0").attr("href",data.articles[0].url)
-       
+        
+        
+    
   
- 
+       
  
         $("#blog-1").text(data.articles[1].title )
-        $("#para-1").text(data.articles[1].content)
+        $("#para-1").text(data.articles[1].description)
         $("#img-1").attr("src", data.articles[1].urlToImage)
         $("#link-1").attr("href",data.articles[1].url)
+
  
  
         $("#blog-2").text(data.articles[2].title)
-        $("#para-2").text(data.articles[2].content)
+        $("#para-2").text(data.articles[2].description)
         $("#img-2").attr("src", data.articles[2].urlToImage)
         $("#link-2").attr("href",data.articles[2].url)
  
